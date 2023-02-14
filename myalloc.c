e07fb81bc5f1f12507b5e62e4db8bf5c80ca91f8
KunalJhatta <Kunaljhatta22@gmail.com>
2023-02-14 15:41:14 -0800

    finished project

--- myalloc.c
+++ myalloc.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/mman.h>
-#include "myalloc.h"
+
+#define ALIGNMENT 16  // Must be power of 2
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x)-1) & (ALIGNMENT - 1)))
+
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
+#define PTR_OFFSET(p, offset) ((void *)((char *)(p) + (offset)))
+
+struct block {
+    struct block *next;
+    int size;    // bytes
+    int in_use;  // bool
+};
 
 struct block *head = NULL;
 
